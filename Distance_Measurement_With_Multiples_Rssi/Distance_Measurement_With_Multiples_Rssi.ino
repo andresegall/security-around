4717e762c868d757fccb98c2fffb3d6d317d70ba
André Vinícius Segall Ando <andre.ando@hbsis.com.br>
2021-07-29 15:53:48 -0300

    Atualização da medição de rssi com múltiplos aparelhos

--- Distance_Measurement_With_Multiples_Rssi/Distance_Measurement_With_Multiples_Rssi.ino
+++ Distance_Measurement_With_Multiples_Rssi/Distance_Measurement_With_Multiples_Rssi.ino
@@ -2,36 +2,44 @@
 #include "SSD1306.h"
 
 SSD1306 display(0x3c, 5, 4);
+char *forkLiftSsids = "ESP32_ACCESS_POINT Zhone_4D23 Segall";
   
 void setup() {
-  Serial.begin(115200);
   display.init();
   display.clear();
 }
 
 void loop() {
   byte available_networks = WiFi.scanNetworks(); //escaneia as redes
-  display.clear();
+  int forkLifts = 0;
 
   for (int network = 0; network < available_networks; network++) {
-    if(WiFi.SSID(network) == "ESP32_ACCESS_POINT") {
-      display.setFont(ArialMT_Plain_16);
-      display.drawString(0, 0, "PERIGO");
-      
-      display.setFont(ArialMT_Plain_10);
-      display.drawString(0, 30, "Empilhadeira 01");
+    char ssid[sizeof(WiFi.SSID(network))];
+
+    for(int i=0; i<sizeof(WiFi.SSID(network)); i++) {
+      ssid[i] = WiFi.SSID(network)[i];
     }
     
-    if(WiFi.SSID(network) == "Zhone_4D23") {
-      display.setFont(ArialMT_Plain_16);
+    if((strstr(forkLiftSsids, ssid) != NULL) && (WiFi.RSSI(network) > -45)) {
+      display.clear();
+      forkLifts++;
+
+      display.setFont(ArialMT_Plain_24);
       display.drawString(0, 0, "PERIGO");
+
+      char number[99];
+      itoa(forkLifts, number, 10);
       
-      display.setFont(ArialMT_Plain_10);
-      display.drawString(0, 30, "Empilhadeira 02");
+      display.setFont(ArialMT_Plain_24);
+      display.drawString(0, 30, number);
+      display.display();
     }
   }
 
-  display.display();
-  
-  Serial.println("----------------------------------------------------");
+  if(forkLifts == 0) {
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 0, "SEGURO");
+    display.display();
+  }
 }
