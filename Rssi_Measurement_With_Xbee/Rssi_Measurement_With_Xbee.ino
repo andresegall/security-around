64141cae8bf6adeacdf11a0526530309184126ca
André Vinícius Segall Ando <andre.ando@hbsis.com.br>
2021-08-18 14:31:14 -0300

    Atualização dos códigos do Xbee

--- Rssi_Measurement_With_Xbee/Rssi_Measurement_With_Xbee.ino
+++ Rssi_Measurement_With_Xbee/Rssi_Measurement_With_Xbee.ino
@@ -3,52 +3,69 @@
 
 SoftwareSerial mySerial(2, 3);
 String rssiMonitor;
-int count = 0;
+int messagePackage = 0;
+int retries = 0;
+int noSignalReceived = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
-  Serial.println("End-point");
   Serial.println("Valor de RSSI:");
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  if (mySerial.available())
-  { 
-    if(count == 6)
-    {
-      delay(10);
-      rssiMonitor = mySerial.read();
-      Serial.println(rssiMonitor);
+void loop() {
+  delay(10);
+  noSignalReceived++;
+  
+  if(mySerial.available()) { 
+    noSignalReceived = 0;
+     
+    if(messagePackage == 6) {
+      int rssi = getRssi();            
 
-      char rssiChar[rssiMonitor.length()+1];//Instanciando array de char
-      strcpy(rssiChar, rssiMonitor.c_str());//Convertendo a string pro array de char
-      int rssi = atoi(rssiChar);            //Convertendo o array de char em int
-
-      if(rssi < 37 && rssi != 0)
-      {
-        digitalWrite(LED_BUILTIN, HIGH);
+      if(rssi < 47 && rssi != 0) {
+        turnOnSignal();
       }
-      else
-      {
-        digitalWrite(LED_BUILTIN, LOW);
+      else {
+        retryOrTurnOffSignal();
       }
-    }
-    else
-    {
+    } 
+    else {
       mySerial.read();
     }
 
-    if(count == 10)
-    {
-      count = 0;
-    }
-    else
-    {
-      count++;
-    }
+    nextPackage();
   }
+
+  if(noSignalReceived == 100) {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+int getRssi() {
+  rssiMonitor = mySerial.read();
+  Serial.println(rssiMonitor);
+  
+  char rssiChar[rssiMonitor.length()+1];  //Instanciando array de char
+  strcpy(rssiChar, rssiMonitor.c_str());  //Convertendo a string pro array de char
+  return atoi(rssiChar);                  //Convertendo o array de char em int
+}
+
+void turnOnSignal() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  retries = 0;
+}
+
+void retryOrTurnOffSignal() {
+  if(retries < 10) {
+    retries++;
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void nextPackage() {
+  messagePackage == 10 ? messagePackage = 0 : messagePackage++;
 }
---
+++ Xbee_Series1_Tx/Xbee_Series1_Tx.ino
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2009 Andrew Rapp. All rights reserved.
+ *
+ * This file is part of XBee-Arduino.
+ *
+ * XBee-Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBee-Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include <XBee.h>
+
+/*
+This example is for Series 1 XBee
+Sends a TX16 or TX64 request with the value of analogRead(pin5) and checks the status response for success
+Note: In my testing it took about 15 seconds for the XBee to start reporting success, so I've added a startup delay
+*/
+
+XBee xbee = XBee();
+
+unsigned long start = millis();
+
+// allocate two bytes for to hold a 10-bit analog reading
+uint8_t payload[] = { 0, 0 };
+
+// with Series 1 you can use either 16-bit or 64-bit addressing
+
+// 16-bit addressing: Enter address of remote XBee, typically the coordinator
+Tx16Request tx = Tx16Request(0x0000, payload, sizeof(payload));
+
+int pin5 = 0;
+int statusLed = 11;
+
+void flashLed(int pin, int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(pin, HIGH);
+      delay(wait);
+      digitalWrite(pin, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
+}
+
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+}
+
+void loop() {
+   // start transmitting after a startup delay.  Note: this will rollover to 0 eventually so not best way to handle
+    if (millis() - start > 500) {
+      // break down 10-bit reading into two bytes and place in payload
+      pin5 = analogRead(5);
+      payload[0] = pin5 >> 8 & 0xff;
+      payload[1] = pin5 & 0xff;
+      
+      xbee.send(tx);
+
+      // flash TX indicator
+      flashLed(statusLed, 1, 100);
+    }   
+    delay(100);
+}
